8c08fbcb53130d8b407f49f52f3a097bb40a6f8f
labuba2410 <vitalyavadaev07202006@gmail.com>
2025-09-12 20:31:43 +0300

    Добавьте файлы проекта.

---
+++ avadaev_lab1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Pipe {
+    string name;
+    float length;
+    int diametr;
+    bool status;
+};
+
+struct CS {
+    string name;
+    int number_work;
+    int number_work_online;
+    string class_cs;
+};
+
+void ShowMenu(Pipe t, CS cs)
+{
+    int option;
+    while (1) {
+        cout << "Chooose options: \n1. Add pipe \n2. Add CS \n3. Show all objects";
+        cin >> option;
+        switch (option) {
+        case 1:
+            cout << "Insert pipe name pls: ";
+            cin >> t.name;
+            break;
+        case 2:
+            break;
+        case 3:
+            cout << "\nPipe name: " << t.name;
+            break;
+        }
+    }
+};
+
+int main()
+{
+    cout << "Hello World!\n";
+    Pipe truba;
+    CS comp_station;
+    ShowMenu(truba, comp_station);
+    return 1;
+}
+
+//Ввод имени трубы с пробелом, отрицательные значения, нулевое значение в кол-ве цехов и др
+//
